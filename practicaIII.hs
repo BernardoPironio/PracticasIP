376ee5de240e16634a6068f8a97a8f7e4fdb31af
Bernardo Pironio <bpironio@gmail.com>
2026-08-27 23:43:35 -0300

    hasta el 3

--- practicaIII.hs
+++ practicaIII.hs
@@ -1,3 +1,5 @@
+import Data.Ratio
+
 {-
   =========================================
   EJERCICIO 1
@@ -35,10 +37,57 @@ absoluto x | x < 0  = -x
            | x >= 0 = x
 
 -- (b)
-maximoAbsoluto:: Integer -> Integer
+maximoAbsoluto:: Integer -> Integer -> Integer
 maximoAbsoluto x y  | absoluto x < absoluto y  = absoluto y
                     | absoluto x >= absoluto y = absoluto x
 
 -- (c)
-maximo3:: Integer -> Integer 
-maximo3 x y z   |
+maximo3:: Integer -> Integer -> Integer -> Integer 
+maximo3 x y z   | x >= y && x >= z = x
+                | y >= x && y >= z = y
+                | z >= x && z >= y = z
+
+-- (d)
+algunoEsCero:: Float -> Float -> Bool 
+algunoEsCero x y    | x == 0 || y == 0   = True
+                    | otherwise          = False
+
+-- (e)
+ambosSonCero:: Float -> Float -> Bool
+ambosSonCero x y    | x == 0 && y == 0 = True
+                    | otherwise        = False
+
+-- (f)
+enMismoIntervalo:: Float -> Float -> Bool
+enMismoIntervalo x y    | x <= 3 && y <= 3                  = True
+                        | x <= 7 && x > 3 && y <= 7 && y >3 = True
+                        | x > 7 && y > 7                    = True
+                        | otherwise                         = False
+
+-- (g)
+sumaDistintos:: Integer -> Integer -> Integer -> Integer
+sumaDistintos x y z | x /= y && x /= z && y /= z  = x + y + z
+                    | x == y && x /= z && y /= z  = z
+                    | x /= y && x == z && y /= z  = y
+                    | x /= y && x /= z && y == z  = x 
+                    | x == y && x == z && y == z  = 0
+
+-- (h)
+esMultiploDe:: Integer -> Integer -> Bool
+esMultiploDe x y = mod x y == 0 
+
+-- (i)
+digitoUnidades:: Integer -> Integer
+digitoUnidades x = mod x 10
+
+-- (j)
+digitoDecenas:: Integer -> Integer
+digitoDecenas x = digitoUnidades (div x 10)
+
+{-
+  =========================================
+  EJERCICIO 3
+  =========================================
+-}
+
+      
