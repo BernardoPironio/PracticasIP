8a7697c0338ab6d5f5d0381009ea5054cbc668bb
Bernardo Pironio <bpironio@gmail.com>
2026-09-07 16:34:04 -0300

    hasta el P4E8 + 14,16abd

--- practicaIV.hs
+++ practicaIV.hs
@@ -90,8 +90,70 @@ sumaDigitos n | div n 10 == 0  = mod n 10
   =========================================
 -}
 
-esCapicua:: Integer -> Bool
-esCapicua n | div n 10 == 0 = True
-            | otherwise     = mod n 10 == mod (div n (10^((cantDigitos n) - 1))) 10 -- && esCapicua (div n 10) 
+--esCapicua:: Integer -> Bool
+--esCapicua n | div n 10 == 0 = True
+--            | otherwise     = mod n 10 == mod (div n (10^((cantDigitos n) - 1))) 10 -- && esCapicua (div n 10) 
+
+{-
+  =========================================
+  EJERCICIO 14
+  =========================================
+-}
+sumaPotencias:: Integer -> Integer -> Integer -> Integer
+sumaPotencias q n m = (sumaN q n)*(sumaN q m)
+  where
+    sumaN q n | n == 1 = q
+          | otherwise = q^n + sumaN q (n-1) 
+
+{-
+  =========================================
+  EJERCICIO 16
+  =========================================
+-}
+
+-- (a)
+menorDivisor:: Integer -> Integer 
+menorDivisor n  | n == 1    = 1
+                | otherwise = menorDivisorDesde n 2 
+  where
+    menorDivisorDesde n k | mod n k == 0 = k 
+                          | otherwise    = menorDivisorDesde n (k + 1)
+
+-- (b)
+esPrimo:: Integer -> Bool
+esPrimo n | n == 1              = False
+          | menorDivisor n == n = True
+          | otherwise           = False
+
+-- (c)
+--sonCoprimos:: Integer -> Integer -> Bool
+--sonCoprimos n k | menorDivisor n == menorDivisor k
+--                | otherwise = menorDivisorDesde n n-1 
+--  where
+--    menorDivisorDesde n k | mod n k == 0 = k 
+--                          | otherwise    = menorDivisorDesde n (k + 1)
+
+
+-- (d)
+nEsimoPrimo:: Integer -> Integer
+nEsimoPrimo n  | n == 1 = 2
+            | otherwise = siguientePrimoDesde (nEsimoPrimo (n-1) + 1)
+  where 
+    siguientePrimoDesde n | esPrimo n = n
+                          | otherwise = siguientePrimoDesde (n+1)
+
+{-
+  =========================================
+  EJERCICIO 19
+  =========================================
+-}
+
+esSumaInicialDePrimos:: Integer -> Bool
+esSumaInicialDePrimos n | n - sumaPrimos(1) < 0 = False
+                        | otherwise = n == 
+ 
+sumaPrimos:: Integer -> Integer 
+sumaPrimos n | n == 0 = 0
+              | otherwise = nEsimoPrimo n + sumaPrimos (n-1) 
 
 
